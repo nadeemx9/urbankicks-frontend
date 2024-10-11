b7faf94074fe0b4b24c435c2a44539cab707053d
nadeem_palkhiwala <nadeem.palkhiwala@xomicmail.com>
2024-10-11 16:06:06 +0530

    Resolved expired JWT Token issue.

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,4 +1,5 @@
-import { HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
   const token = localStorage.getItem('token');  // Retrieve token from localStorage directly
@@ -7,7 +8,17 @@ export const authInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) =>
     const clonedRequest = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
     });
-    return next(clonedRequest);
+    return next(clonedRequest).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          // Handle token expiration or unauthorized access
+          localStorage.removeItem('token'); // Remove expired token
+          // Redirect to login
+          window.location.href = '/login';
+        }
+        return throwError(() => new Error(error.message));
+      })
+    );
   }
   return next(req);
 };
